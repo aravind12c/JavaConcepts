53bed752dc611ae34ca09366e4563e5b482f4682
Aravind Chandrasekaran <65638184+aravind12c@users.noreply.github.com>
2021-08-18 23:39:23 +0530

    Add files via upload

---
+++ Sub_Array/Sub_Array.java
@@ -0,0 +1,38 @@
+package Sub_Array;
+
+public class Sub_Array {
+
+	static void printSubArrays(int []arr, int start, int end) 
+	{      
+	    // Stop if we have reached the end of the array      
+	    if (end == arr.length)  
+	        return; 
+	      
+	    // Increment the end point and start from 0  
+	    else if (start > end)  
+	        printSubArrays(arr, 0, end + 1); 
+	          
+	    // Print the subarray and increment the starting point  
+	    else
+	    { 
+	        System.out.print("["); 
+	        for (int i = start; i < end; i++){ 
+	            System.out.print(arr[i]+", "); 
+	        } 
+	          
+	        System.out.println(arr[end]+"]"); 
+	        printSubArrays(arr, start + 1, end); 
+	    } 
+	      
+	    return; 
+	} 
+	  
+	public static void main(String args[]) 
+	{ 
+	int []arr = {2,4}; 
+	printSubArrays(arr, 0, 0); 
+	  
+	} 
+
+	
+}
